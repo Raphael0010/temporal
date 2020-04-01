b6caaa2b12a3eff93c47f244802d66ae24c92e64
Raphael Mortier <raphaelmortier@outlook.Fr>
2020-04-01 15:43:49 +0200

    feat(api getEmail)

--- src/api/api.py
+++ src/api/api.py
@@ -2,6 +2,7 @@ import threading
 import os
 import subprocess
 import sys
+import json
 import time
 from flask import Flask, json, jsonify, request
 from flask_cors import CORS
@@ -11,15 +12,53 @@ CORS(api)
 
 
 def apiRunThread():
-    api.run(host='172.22.247.155', port=8888)
+    api.run(host='127.0.0.1', port=8888)
+    # api.run(host='172.22.247.155', port=8888)
 
 
 @api.route('/api/email/get/<email>', methods=['GET'])
 def getEmail(email):
-    order = "doveadm fetch -u usermail 'text' TO {}@temporal.com".format(email)
+    order = "doveadm fetch -u usermail 'text' TO {}@temporal.com".format(
+        email)
     cmd = subprocess.Popen(order, shell=True, stdout=subprocess.PIPE,
                            stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-    return cmd
+
+    data = "["
+    for x in cmd.stdout.split("text:"):
+        # si il trouve un sujet dans le bloc
+        if(x.find("Subject:") != -1):
+            # parse du sujet
+            sujet = x[x.find("Subject:"):x.find("From:")]
+            sujet = sujet.replace('Subject: ', '')
+
+            # parse de la date
+            date = x[x.find("Date:"):x.find("Date:") + 37]
+            date = date.replace('Date: ', '')
+
+            # parse de la from
+            fromEmail = x[x.find("From:"):x.find("To: <")]
+            fromEmail = fromEmail.replace('From: ', '')
+
+            # parse du message
+            message = x[x.find("Date:") + 46:]
+            message = message.replace(
+                "Vous avez du courrier dans /var/mail/usermail", "")
+
+            ligne = {
+                "from": fromEmail.rstrip(),
+                "date": date.rstrip(),
+                "sujet": sujet.rstrip(),
+                "message": message.rstrip()
+            }
+
+            if(data == "["):
+                data = data + json.dumps(ligne)
+            else:
+                data = data + "," + json.dumps(ligne)
+        else:
+            continue
+
+    return data + "]"
 
 
 @api.route('/api/email/send', methods=['POST'])
